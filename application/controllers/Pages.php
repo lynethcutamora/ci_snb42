7c3d4ad7516eaf04c6dd281f13742ae23747fdcd
string44 <jason.pitogo1@gmail.com>
2015-12-27 17:30:48 +0800

    heto na

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1242,31 +1242,26 @@ class Pages extends CI_Controller {
 	public function message($msgId=null)
 	{
 		if(($this->session->userdata('userId')!=""))
-		{
+		{	
 			if(isset($msgId))
+			{	
+				$data['msgId'] = $msgId;
+
+			}else
 			{
+				$data['msgId'] = $this->post->firstMsg($msgId);
+			}
 				$query=$this->_userData();
 				$data['data']=$query->result_array();
 				$data['pages']='message';
 				$data['countgroup'] = $this->countGroups();
 				$groupquery= $this->groupdetails();
 				$data['groupdetails'] = $groupquery->result_array();
-				$this->load->view('pages/dashboard/fixed',$data);
-				$this->load->view('pages/message/content'); 
-				$this->load->view('pages/dashboard/controlsidebar');
-				$this->load->view('pages/dashboard/end');
-			}else{
-				$query=$this->_userData();
-				$data['data']=$query->result_array();
-				$data['pages']='message';
-				$data['countgroup'] = $this->countGroups();
-				$groupquery= $this->groupdetails();
-				$data['groupdetails'] = $groupquery->result_array();
+				
 				$this->load->view('pages/dashboard/fixed',$data);
 				$this->load->view('pages/message/newcontent'); 
 				$this->load->view('pages/dashboard/controlsidebar');
 				$this->load->view('pages/dashboard/end');
-			}
 		}else
 		{
 			$this->_landing();
