0e40c616f619a370e15e105c9f9fca98d60e6151
lynethcutamora <lyneth.cutamora@gmail.com>
2015-12-30 14:39:50 +0800

    add investor

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -282,8 +282,12 @@ class Pages extends CI_Controller {
 					$data['groupDtl'] = $groupDetails->result_array();
 					$memberinfo= $this->memberinfo($groupId);
 					$data['memberinfo'] = $memberinfo->result_array();
+					$investorinfo= $this->investorinfo($projectId);
+					$data['investorinfo'] = $investorinfo->result_array();
 					$search= $this->_searchpeople();
 					$data['searchpeople'] = $search->result_array();
+					$searchinvestor= $this->_searchinvestor();
+					$data['searchinvestor'] = $searchinvestor->result_array();
 				}
 			}			
 			else $this->index();
@@ -953,17 +957,26 @@ class Pages extends CI_Controller {
 	public function _searchpeople(){
 		$this->db->select('*');
 		$this->db->from('user_md a');
-		$this->db->join('user_dtl b','a.userId=b.userId','left');
 		$this->db->join('company_dtl c','a.userId=c.userId','left');
+		$this->db->join('user_dtl b','a.userId=b.userId','left');
 		$this->db->like('user_fName',$this->input->post('txtsearch'),'both');
 		$this->db->or_like('user_lName',$this->input->post('txtsearch'),'both');
-		$this->db->like('company_name',$this->input->post('txtsearch'),'both');
+		$this->db->or_like('company_name',$this->input->post('txtsearch'),'both');
 		$query=$this->db->get();
 
 		return $query;
 	}
 	
+	public function _searchinvestor(){
+		$this->db->select('*');
+		$this->db->from('user_md a');
+		$this->db->join('user_dtl b','a.userId=b.userId','left');
+		$this->db->like('user_fName',$this->input->post('txtsearchinvestor'),'both');
+		$this->db->or_like('user_lName',$this->input->post('txtsearchinvestor'),'both');
+		$query=$this->db->get();
 
+		return $query;
+	}
 	public function memberinfo($groupid){
 		$this->db->select('*');
 		$this->db->from('group_ext a');
@@ -976,6 +989,18 @@ class Pages extends CI_Controller {
 		return $query;
 	}
 
+	public function investorinfo($projectid){
+		$this->db->select('*');
+		$this->db->from('investor_dtl a');
+		$this->db->join('avatar_dtl b', 'a.userId=b.userId','left');
+		$this->db->join('badge_dtl c', 'a.userId=c.userId', 'left');
+		$this->db->join('user_dtl e', 'a.userId=e.userId', 'left');
+		$this->db->where('a.postId',$projectid);
+		$query=$this->db->get();
+
+		return $query;
+	}
+
 	public function addmember(){
 		$data = array(
 				'groupId' => $this->input->post('groupid'),
@@ -983,7 +1008,7 @@ class Pages extends CI_Controller {
 			);
 
 		$this->db->insert('group_ext',$data);
-		$this->group( $this->input->post('groupid'));
+		header('Location:'.base_url().'pages/group/'.$this->input->post('groupid'));
 	}
 
 	public function addinvestor(){
@@ -993,7 +1018,7 @@ class Pages extends CI_Controller {
 			);
 		
 		$this->db->insert('investor_dtl',$data);
-		$this->group( $this->input->post('groupid'));
+		header('Location:'.base_url().'pages/group/'.$this->input->post('groupid').'/'.$this->input->post('projectid'));
 	}
 
 	public function badge()
