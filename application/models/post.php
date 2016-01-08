e46df18d4445d54315e1e6b40ef2dbfc444d792a
lynethcutamora <lyneth.cutamora@gmail.com>
2016-01-09 01:20:49 +0800

    detect bad comments

--- application/models/post.php
+++ application/models/post.php
@@ -402,12 +402,12 @@
                 return 'true';
         }
 
-        public function autosearchduplicate($text){
-            $sql = "SELECT postTitle, postContent 
-                    FROM userpost
-                    WHERE MATCH (postTitle) AGAINST (?)>0";
-            $query=$this->db->query($sql,array($text,$text));
-            return $query->result();
-        }
+        // public function autosearchduplicate($text){
+        //     $sql = "SELECT postTitle, postContent 
+        //             FROM userpost
+        //             WHERE MATCH (postTitle) AGAINST (?)>0";
+        //     $query=$this->db->query($sql,array($text,$text));
+        //     return $query->result();
+        // }
     }
 ?>
