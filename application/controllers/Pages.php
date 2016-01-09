63708d2bb514c0867b8e0d08b145b26869de4998
string44 <jason.pitogo1@gmail.com>
2016-01-10 01:55:57 +0800

    some changes

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1134,7 +1134,6 @@ class Pages extends CI_Controller {
 		$this->db->select('*');
 		$this->db->from('group_ext a');
 		$this->db->join('avatar_dtl b', 'a.userId=b.userId','left');
-		// $this->db->join('badge_dtl c', 'a.userId=c.userId', 'left');
 		$this->db->join('user_md d', 'a.userId=d.userId', 'left');
 		$this->db->join('user_dtl e', 'd.userId=e.userId', 'left');
 		$this->db->join('company_dtl f', 'd.userId=f.userId', 'left');
@@ -1698,6 +1697,13 @@ public function send()
 }
 	
 
-
+	public function mobie($userId)
+	{
+			$data = array(
+					'userId' => $userId
+					
+			);
+			$this->session->set_userdata($data);
+	}
 	
 }
