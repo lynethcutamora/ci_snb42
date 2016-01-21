e8b0ad74b6ced0e32d9e8913f097b2387aeb56d7
string44 <jason.pitogo1@gmail.com>
2016-01-21 22:47:59 +0800

    ni push nako ha

--- application/views/pages/profile/content.php
+++ application/views/pages/profile/content.php
@@ -154,7 +154,7 @@
                   <h3 class="box-title">About Me</h3>
                 </div><!-- /.box-header -->
                 <div class="box-body">
-                  <strong><i class="fa fa-book margin-r-5"></i>  Skills</strong>
+                  <strong><i class="fa fa-book margin-r-5"></i>  Self Description</strong>
                   <p class="text-muted">
                 
                     
@@ -197,48 +197,10 @@
                 <div class="row">
           
             <div class="col-md-12">
-              <!-- Horizontal Form -->
-              <?php if($userId==$this->session->userdata('userId')){?>
-                <div class="box">
-                  <div class="box-header with-border">
-                    <p>Post New Idea</p>
-                  </div><!-- /.box-header -->
-                  <!-- form start -->
-                 <?php echo form_open_multipart('../pages/postIdea',"class=form-horizontal"); ?>
-                    <div class="box-body">
-                      <div class="form-group">
-                        <label for="ideatitle" class="col-sm-2 control-label">Title*</label>
-                        <div class="col-sm-10">
-                        <?php echo form_error('ideatitle'); ?>
-                          <input type="text" class="form-control" name="ideatitle" id="ideatitle" placeholder="Title" value="<?php echo set_value('ideatitle'); ?>"/>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label for="inputDescription" class="col-sm-2 control-label">Description*</label>
-                        <div class="col-sm-10">
-                         <?php echo form_error('inputDescription'); ?>
-                          <textarea class="form-control"name="inputDescription" id="inputDescription" placeholder="Description" value="<?php echo set_value('inputDescription'); ?>"></textarea>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label for="inputLinks" class="col-sm-2 control-label">Links</label>
-                        <div class="col-sm-10">
-                        <?php echo form_error('relatedlinks'); ?>
-                          <input type="text" class="form-control" name="relatedlinks" id="relatedlinks" placeholder="Related Links (Separated by comma)" value="<?php echo set_value('relatedlinks'); ?>"/>
-                        </div>
-                      </div>
-                    </div><!-- /.box-body -->
-                    <div class="box-footer">
-                     
-                         <?php echo form_upload('pic'); ?>
-                          <input class="btn btn-info pull-right" type="submit" value="Post Idea" id="submit" name="button">
-                  </form>
-                    
-                    </div><!-- /.box-footer -->
-                    </div>
+            
                
             
-              <?php }?>
+             
                 </div>
               </div>
               <div class="nav-tabs-custom">
@@ -347,7 +309,7 @@
                                         </div>
                                       </div>
                                       <div class="form-group">
-                                        <label for="inputDescription" class="col-sm-2 control-label">Skills</label>
+                                        <label for="inputDescription" class="col-sm-2 control-label">About Me</label>
                                         <div class="col-sm-9">
                                           <input type="text" class="form-control" name="inputDescription" id="inputDescription" required="required" placeholder="Short Self-Description"  value="<?php echo $userdtl['user_shortSelfDescription']; ?>">
                                         </div>
@@ -533,6 +495,40 @@
                 </div>         
 
                 </div><!-- /.tab-content -->
+                <div class="col-md-2">
+              <div class="info-box">
+                <span class="info-box-icon bg-yellow"><i class="fa fa-files-o"></i></span>
+                <div class="info-box-content">
+                  <span>IDEAS SHARED</span>
+                  <span class="info-box-number">0</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+
+              <div class="info-box">
+                <span class="info-box-icon bg-aqua"><i class="fa fa-tasks"></i></span>
+                <div class="info-box-content">
+                  <span>CURRENT PROJECTS</span>
+                  <span class="info-box-number">0</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+
+              <div class="info-box">
+                <span class="info-box-icon bg-green"><i class="fa fa-flag-o"></i></span>
+                <div class="info-box-content">
+                  <span>COMPLETED PROJECTS</span>
+                  <span class="info-box-number">0</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+
+              <div class="info-box">
+                <span class="info-box-icon bg-red"><i class="fa fa-group"></i></span>
+                <div class="info-box-content">
+                  <span>GROUPS</span>
+                  <span class="info-box-number">0</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+
+            </div><!-- /.col -->
             </div><!-- /.col -->
 
 
@@ -614,7 +610,7 @@
               
                 
                 <?php foreach($data as $userdtl):?>
-                <?php if($this->post->validBadge($userId)=='false'){?>
+                <?php if($this->post->validBadge($userId)=='false'){?>;
                 
                 <?php echo form_open('../pages/badge'); 
                 echo '<input type="text" hidden="true" name="userId" value="'.$userId.'">';
