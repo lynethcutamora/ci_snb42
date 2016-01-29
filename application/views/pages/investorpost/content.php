67b0e3bacfa83b8be5c83d1dd7651823becc0a99
string44 <jason.pitogo1@gmail.com>
2016-01-30 07:43:24 +0800

    upvote

--- application/views/pages/investorpost/content.php
+++ application/views/pages/investorpost/content.php
@@ -243,15 +243,7 @@
     </div><!-- /.box-->
 
           <?php } ?>
-          <table id="example2">
-                    <thead>
-                   
-                        <th>Users</th>
-                        <th></th>
-                    </thead>
-                   
-                    <tbody>
-                      <tr>
+         
 
               
           <div name="investorpost" id="investorpost"></div>
