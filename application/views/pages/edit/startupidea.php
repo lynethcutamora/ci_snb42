7c34d7992d6cdd1c655f1d7af1e0a39e0834f2ba
lynethcutamora <lyneth.cutamora@gmail.com>
2016-02-06 21:29:29 +0800

    edit startup idea

--- application/views/pages/edit/startupidea.php
+++ application/views/pages/edit/startupidea.php
@@ -58,9 +58,68 @@
                         <label for="inputLinks" class="col-sm-2 control-label">Links</label>  
                         <div class="col-sm-10">
                         <?php echo form_error('relatedlinks'); ?>
-                          <input type="text" class="form-control" name="relatedlinks" id="relatedlinks" placeholder="Related Links (Separated by comma)" value="<?php echo $this->post->getRelatedLinks($row['postId']); ?>"/>
+                          <input type="text" class="form-control" name="relatedlinks" id="relatedlinks" placeholder="Related Links (Separated by comma)" <?php echo "value=\"".$this->post->getLinkEdit($row['postId'])."\"" ?> />
                         </div>
                       </div>
+
+                    <?php       if($this->post->checkWithBmc($row['postId'])==true){
+
+                    foreach($this->post->bmcquery($row['postId'])->result_array() as $row1): ?>
+                             <div class="col-md-12">
+                                  <div class="box box-default ">
+                                    <div class="box-header with-border">
+                                      <h3 class="box-title"><small>Business Model Canvas</small></h3>
+                                      <div class="box-tools pull-right">
+                                      
+                                      </div><!-- /.box-tools -->
+                                    </div><!-- /.box-header -->
+                                  <div class="box-body">
+                                    <table class="table table-bordered table-hover">
+                                      <thead>
+                                        <tr>
+                                          <th><small>Key Partners</small></th>
+                                          <th><small>Key Activities</small></th>
+                                          <th><small>Values Propositions</small></th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                        <tr>
+                                          <td><textarea class="form-control"name="inputKeyPartners" id="inputKeyPartners" <?php  if($row1['key_partners']==null)echo "placeholder=\"Who are out Key Partners?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['key_partners']); }?></textarea></td>
+                                          <td><textarea class="form-control"name="inputKeyActivities" id="inputKeyActivities" <?php  if($row1['key_activities']==null)echo "placeholder=\"What Key activities do our Value Propositions requires?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['key_activities']); }?></textarea></td>
+                                          <td><textarea class="form-control"name="inputValuePropositions" id="inputValuePropositions" <?php  if($row1['value_proposition']==null)echo "placeholder=\"What value do we deliver to the customer?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['value_proposition']); }?></textarea></td>
+                                        </tr>
+                                        <tr>
+                                          <th><small>Customer Relationships</small></th>
+                                          <th><small>Customer Segments</small></th>
+                                          <th><small>Key Resources</small></th>
+                                        </tr>
+                                        <tr>
+                                          <td><textarea class="form-control"name="inputCustomerRelationship" id="inputCustomerRelationship" <?php  if($row1['customer_relationships']==null)echo "placeholder=\"What type of relationship does each of our Customer Segments expect us to establish and maintain with them?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['customer_relationships']); }?></textarea></td>
+                                          <td><textarea class="form-control"name="inputCusomerSegments" id="inputCusomerSegments" <?php  if($row1['customer_segments']==null)echo "placeholder=\"From whom are we creating value?\" >"; else { echo ">"; $this->post->bmcexplode($row1['customer_segments']); }?></textarea></td>
+                                          <td><textarea class="form-control"name="inputKeyResources" id="inputKeyResources" <?php  if($row1['key_resources']==null)echo "placeholder=\"What Key Resources do our Value Propositions require?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['key_resources']); }?></textarea></td>
+                                        </tr>
+                                        <tr>
+                                          <th><small>Channels</small></th>
+                                          <th><small>Cost Structure</small></th>
+                                          <th><small>Revenue Streams</small></th>
+                                        </tr>
+                                        <tr>
+                                          <td><textarea class="form-control"name="inputChannels" id="inputChannels" placeholder="Through what channel do our Customer Segments want to be reached?"></textarea></td>
+                                          <td><textarea class="form-control"name="inputCostStructure" id="inputCostStructure" placeholder="What are the most important costs inherent in our business model?"></textarea></td>
+                                          <td><textarea class="form-control"name="inputRevenueStreams" id="inputRevenueStreams" placeholder="For what value are our customers willing to pay?"></textarea></td>
+                                        </tr>
+                                        <tr>
+                                          <td><textarea class="form-control"name="inputChannels" id="inputChannels" <?php  if($row1['channels']==null)echo "placeholder=\"What type of relationship does each of our Customer Segments expect us to establish and maintain with them?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['channels']); }?></textarea></td>
+                                          <td><textarea class="form-control"name="inputCostStructure" id="inputCostStructure" <?php  if($row1['cost_structure']==null)echo "placeholder=\"From whom are we creating value?\" >"; else { echo ">"; $this->post->bmcexplode($row1['cost_structure']); }?></textarea></td>
+                                          <td><textarea class="form-control"name="inputRevenueStreams" id="inputRevenueStreams" <?php  if($row1['revenue_streams']==null)echo "placeholder=\"What Key Resources do our Value Propositions require?\" >"; else { echo ">"; echo $this->post->bmcexplode($row1['revenue_streams']); }?></textarea></td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </div><!-- /.box-body -->
+                                </div><!-- /.box -->
+                              </div><!-- /.col-12 -->
+                      <?php endforeach; ?>
+                    <?php }else{ ?>
                       <div class="col-md-12">
                         <div class="box box-default collapsed-box">
                           <div class="box-header with-border">
@@ -109,6 +168,8 @@
                           </div><!-- /.box-body -->
                         </div><!-- /.box -->
                       </div><!-- /.col -->
+                  <?php } ?>
+
                     </div><!-- /.box-body -->
                     <div class="box-footer">
                      
@@ -117,10 +178,23 @@
                           <button class="btn btn-info pull-right" type="submit" id="postnewidea" name="postnewidea">Update Idea</button>
                     </div>
                   </form>
-            </div>
-          </div><!-- /.box -->
-          <?php endforeach; ?>
-        </div>
-           
+            </div><!-- /.box-body -->
+          </div><!-- /.box default-->
+        </div><!-- /. col-md-8-->
+        <?php endforeach; ?>
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
+
+<script>
+ function changeCategory(obj){
+   var x = document.getElementById("categorytxt").value;
+
+   if(x=='1'){
+    document.getElementById("optional").style.display="block";
+    document.getElementById("optional1").style.display="block";
+   }else{
+    document.getElementById("optional").style.display="none";
+    document.getElementById("optional1").style.display="none";
+  }
+} 
+</script>
