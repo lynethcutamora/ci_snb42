b2798040dd81f32512778782b70e04e5d0249221
string44 <jason.pitogo1@gmail.com>
2016-02-07 01:16:56 +0800

    fix

--- application/views/pages/edit/startupidea.php
+++ application/views/pages/edit/startupidea.php
@@ -1,3 +1,4 @@
+        <script src="<?php echo base_url(); ?>plugins/jQuery/jQuery-2.1.4.min.js"></script>
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
       <div class="container">
